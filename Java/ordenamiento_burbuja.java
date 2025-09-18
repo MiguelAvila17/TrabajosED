80e2090b7b7155feb08909347849fe0779afc7f2
Miguel Angel <aviladavizon17@gmail.com>
2025-09-17 23:30:46 -0700

    ordenamiento burbuja Java

---
+++ Java/ordenamiento_burbuja.java
@@ -0,0 +1,26 @@
+public class Burbuja {
+    static void burbuja(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        System.out.print("Original: ");
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+
+        burbuja(arr);
+
+        System.out.print("Ordenado: ");
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
