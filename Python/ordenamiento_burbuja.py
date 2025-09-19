673e155275167dfd1b62455b0f8c24e3e5d744fb
Miguel Angel <aviladavizon17@gmail.com>
2025-09-18 22:44:05 -0700

    Actualización

--- Python/ordenamiento_burbuja.py
+++ Python/ordenamiento_burbuja.py
@@ -1,12 +1,14 @@
+import random
+
 def burbuja(arr):
     n = len(arr)
     for i in range(n - 1):
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # intercambio
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-# Ejemplo
-arr = [64, 34, 25, 12, 22, 11, 90]
+# Generar arreglo de 10 números aleatorios entre 1 y 100
+arr = [random.randint(1, 100) for _ in range(10)]
 print("Original:", arr)
 burbuja(arr)
 print("Ordenado:", arr)
