b941c190eadd3083255a86aaca0b50f2bc7d3c1d
Miguel Angel <aviladavizon17@gmail.com>
2025-10-08 00:04:18 -0700

    Hash_Sort.java

---
+++ Java/Hash_Sort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class HashSort {
+    public static void hashSort(List<Integer> arr) {
+        Map<Integer, Integer> hashTable = new HashMap<>();
+        int maxVal = 0;
+
+        for (int num : arr) {
+            hashTable.put(num, hashTable.getOrDefault(num, 0) + 1);
+            if (num > maxVal) maxVal = num;
+        }
+
+        arr.clear();
+        for (int i = 0; i <= maxVal; i++) {
+            if (hashTable.containsKey(i)) {
+                for (int j = 0; j < hashTable.get(i); j++)
+                    arr.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>(Arrays.asList(4, 2, 2, 8, 3, 3, 1));
+
+        System.out.println("Arreglo original: " + arr);
+
+        hashSort(arr);
+
+        System.out.println("Arreglo ordenado: " + arr);
+    }
+}
