95e7c02abdd829d3565980f3047f3b1cd06538fa
Miguel Angel <aviladavizon17@gmail.com>
2026-01-08 14:48:15 -0700

    .

---
+++ proyecto binas terminado/src/json_io.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <memory>
+#include "nodo.h"
+
+std::string serializarJson(const std::shared_ptr<Nodo>& n);
+std::shared_ptr<Nodo> cargarJson(const std::string& contenido);
+
